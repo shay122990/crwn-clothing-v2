9592ab9322f69c723e33d36a0f05514830af53d9
Shay.Asanova <shay.asanova@gmail.com>
2023-01-31 23:31:59 +0400

    removed context and replaced with redux

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <CategoriesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CategoriesProvider>
-    </UserProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
