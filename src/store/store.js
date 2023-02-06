4754e2b3b4e8796ada67bd101398ccaaa17ab3c1
Shay.Asanova <shay.asanova@gmail.com>
2023-02-06 21:41:27 +0400

    debugged logger middleWare for better logs

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
 
-const middleWares = [logger];
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
