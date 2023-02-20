8640108a7bd4bae939ba6de9e732837c7a9cc35d
Shay.Asanova <shay.asanova@gmail.com>
2023-02-20 13:47:46 +0400

    added a spinner to categories-preview and category components as well as whitelisted the cart

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,12 +2,17 @@ import { CategoryContainer, Title } from "./category.styles";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useState, useEffect, Fragment } from "react";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinner.component";
 import ProductCard from "../../components/product-card/product-card.component";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -17,12 +22,16 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner></Spinner>
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
