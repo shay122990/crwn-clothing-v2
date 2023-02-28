72da0eea04b2e2c7c6cd84d194f19258654f536c
Shay.Asanova <shay.asanova@gmail.com>
2023-02-28 17:18:41 +0400

    implemented sagas in category and added root-saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
@@ -11,12 +13,14 @@ const persistConfig = {
   whitelist: ["cart"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //if we're in development, render the logger
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 //setting up for redux devtools (if we're not in production and theres a window object and the devtools exsist then use this compose otherwise use the redux compose)
@@ -33,5 +37,6 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
