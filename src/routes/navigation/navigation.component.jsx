133bcf23166c55eccbf9b55f59106c6716edc91f
Shay.Asanova <shay.asanova@gmail.com>
2023-03-08 20:59:53 +0400

    created suignup and sign out sagas and implemented inside relative components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,7 +4,7 @@ import {
   NavLinks,
   NavLink,
 } from "./navigation.styles";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
@@ -14,13 +14,14 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { signOutStart } from "../../store/user/user.action";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
-
   const isCartOpen = useSelector(selectIsCartOpen);
 
+  const signOutUser = () => dispatch(signOutStart());
   return (
     <Fragment>
       <NavigationContainer>
